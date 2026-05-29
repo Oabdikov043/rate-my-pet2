653137be95a75875f0b246324799ccf236f5704f
osmanabdikov123 <osman.abdikov123@gmail.com>
2026-05-29 04:23:57 +0300

    login

--- rate-my-pet2/src/main.js
+++ rate-my-pet2/src/main.js
@@ -2,11 +2,14 @@ import page from "page";
 import homeView from './views/home';
 import layoutView  from "./views/layout";
 import petsView from "./views/pets";
+import loginView from "./views/login";
 
 page(layoutView);
 
 page("/", homeView);
 
 page('/pets', petsView);
+
+page("/login", loginView);
 
 page();
