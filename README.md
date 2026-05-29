1feee672e829868cc6b2d86be6d9e7897fb314da
osmanabdikov123 <osman.abdikov123@gmail.com>
2026-05-29 04:14:06 +0300

    new items

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ Practice
 
 ## Tech Stack 
 -[x] Vannila Js
+-[x] Vite local dev server
+-[x] Vite Bundling
+-[x] Git Source Control with GitHub
+-[x] Tailwind Css
 
 
--- rate-my-pet2/src/main.js
+++ rate-my-pet2/src/main.js
@@ -1,6 +1,12 @@
 import page from "page";
 import homeView from './views/home';
+import layoutView  from "./views/layout";
+import petsView from "./views/pets";
 
-page("/test-123", homeView);
+page(layoutView);
+
+page("/", homeView);
+
+page('/pets', petsView);
 
 page();
